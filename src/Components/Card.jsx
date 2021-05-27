b24d9a8c13d1f87eb739c241f17f56b00cabf768
arif <arif@jubi.ai>
2021-05-27 08:41:10 +0530

    msg

---
+++ src/Components/Card.jsx
@@ -0,0 +1,43 @@
+
+import React from 'react';
+import Lockicon from './images/lock.png';
+
+function Card(){
+    return(
+        <main>
+            <div className="formbxmain">
+                <div className="cardformbx">
+                    <div className="payementtitle">
+                        <p><span className="payicon"><img src={Lockicon} /></span> Payment</p>
+                    </div>
+                    <section className="formbx">
+                        <div className="inputbx">
+                            <label>Card Holders Name</label>
+                            <input type="text" name="name" placeholder="Name On Card" />
+                        </div>
+                        <div className="inputbx">
+                            <label>Card Number</label>
+                            <input type="text" name="name" placeholder="---- ---- ---- ----" />
+                        </div>
+                        <div className="ccvexpiery flex-container"> 
+                            <div className="inputbx">
+                                <label>CCV</label>
+                                <input type="text" name="name" placeholder="Code" />
+                            </div>  
+                            <div className="inputbx">
+                                <label>EXPIRY</label>
+                                <input type="text" name="name" placeholder="mm/yyyy" />
+                            </div>  
+                        </div>
+                        <div className="inputbx">
+                            <button>Pay Now</button>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </main>
+    );
+}
+
+export default Card;
+
